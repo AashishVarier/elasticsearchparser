30d84997d4e02532b876b8e781426620db9acfca
arvariable <35440657+AashishVarier@users.noreply.github.com>
2022-11-08 09:16:35 +0530

    spring initialized

---
+++ src/main/java/com/arvariable/elasticsearchparser/ElasticsearchparserApplication.java
@@ -0,0 +1,13 @@
+package com.arvariable.elasticsearchparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchparserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arvariable/elasticsearchparser/ElasticsearchparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvariable.elasticsearchparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
