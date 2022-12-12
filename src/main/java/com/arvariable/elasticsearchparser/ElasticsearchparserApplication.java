9311ca43a14492652537ead868a70a30ec892136
arvariable <35440657+AashishVarier@users.noreply.github.com>
2022-12-12 12:11:04 +0530

    Update ElasticsearchparserApplication.java

--- src/main/java/com/arvariable/elasticsearchparser/ElasticsearchparserApplication.java
+++ src/main/java/com/arvariable/elasticsearchparser/ElasticsearchparserApplication.java
@@ -9,5 +9,14 @@ public class ElasticsearchparserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ElasticsearchparserApplication.class, args);
 	}
+/* ToDo
+	1. Connect to elasticsearch
+	2. Fetch data from elastic search based on index name
+	3. Schedule/ interval for fetching of data
+	4. Parse fetched data
+	5. Mail parsed data from mailing list details available on excel
+	6. Logging
+	7. Spring Boot Acuator
+*/ 
 
 }
